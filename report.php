c37829c154712e2034cda63f0a55bda3c4bf7bbf
chicagoist <31587029+chicagoist@users.noreply.github.com>
2020-12-23 17:51:26 +0200

    Update report.php

--- report.php
+++ report.php
@@ -80,7 +80,6 @@
           if (!$table_query) {
           print('Error querying database. НЕТ ТАБЛИЦЫ' . "<br />");
           } else {
-
           print("Таблица customers существует<br />");
           }
          * 
